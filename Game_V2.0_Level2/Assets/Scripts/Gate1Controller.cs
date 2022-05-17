18b7b15505817c788c37a60a91a57eafcdb31aa4
OmarSedkyUI <omar_sedky@outlook.com>
2022-05-18 01:31:30 +0200

    Added Terminal_2 Script For Password

---
+++ Game_V2.0_Level2/Assets/Scripts/Gate1Controller.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate1Controller : MonoBehaviour
+{
+    
+    private Animator anim;
+    public InteractableTerminal it;
+    [SerializeField] private Transform player;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (it.OpenGate())
+        {
+            anim.SetBool("Openning", true);
+        }
+
+        if(player.position.x > 183.6346)
+        {
+            anim.SetBool("Openning", false);
+        }
+    }
+}
