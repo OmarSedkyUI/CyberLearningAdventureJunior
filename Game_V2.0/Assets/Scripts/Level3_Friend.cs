d9d52912fd5d783c7256a213f7a7fe53f8b95aa3
DavidKozman <davidmkozman@gmail.com>
2022-07-01 00:15:36 +0200

    Finished Level 3

--- Game_V2.0/Assets/Scripts/Level3_Friend.cs
+++ Game_V2.0/Assets/Scripts/Level3_Friend.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Level3_Friend : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private GameObject dialogue;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private string[] lines;
+
+
     private SpriteRenderer sp;
+    private int index;
+    private bool oneTime;
 
     private void Start()
     {
         sp = GetComponent<SpriteRenderer>();
+        index = 1;
+        text.text = lines[0];
+        oneTime = true;
     }
+
     // Update is called once per frame
     void Update()
     {
-        if (player.position.x > transform.position.x)
-            sp.flipX = false;
-        else
-            sp.flipX = true;
+
+        if(player.position.x >= 140f)
+        {
+            GameObject.Find("Player").GetComponent<Level3_PlayerMovement>().stop = true;
+            if (GameObject.Find("Main Camera").GetComponent<Level3_CameraController>().destReached && oneTime)
+            {
+                dialogue.SetActive(true);
+                oneTime = false;
+            }
+            Conversation();
+        }
+        
+    }
+
+    void Conversation()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && dialogue.activeSelf)
+        {
+            if (index >= lines.Length)
+            {
+                dialogue.SetActive(false);
+                GameObject.Find("Player").GetComponent<Level3_PlayerMovement>().stop = false;
+            }
+            else
+            {
+                GameObject.Find("Player").GetComponent<Level3_PlayerMovement>().stop = true;
+                text.text = lines[index];
+            }
+            index += 1;
+        }
+       
     }
 }
